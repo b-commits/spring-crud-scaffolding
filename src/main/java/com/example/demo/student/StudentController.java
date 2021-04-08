a8fc0576f4e3570b54cec8bdbd648bd920343dd1
Bartosz Goscicki <s19677@pjwstk.edu.pl>
2021-04-08 23:37:48 +0200

    Add CRUD scaffolding

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,44 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    /**
+     * @param studentService - autowired (automatically instantiated and injected into the constructor).
+     *                          Requires @Service annotation.
+     */
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping()
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId")
+    public void deleteStudent(@PathVariable("StudentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId")
+    public void updateStudent(@PathVariable("studentId") Long studentId,
+                              @RequestParam(required = false) String name,
+                              @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
+    }
+}
